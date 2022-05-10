aca00957af456f51dc51b32960b144d9568fbe8c
Swarup Zachariah <Swarup.zac@gmail.com>
2022-05-10 13:47:25 -0400

    Updates

---
+++ precache-manifest.54a2537aca304a6bed000fe756ea8b3b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0f7a17d78124c4886c4",
+    "url": "/swarup_website/static/css/main.d1ddd16c.chunk.css"
+  },
+  {
+    "revision": "b0f7a17d78124c4886c4",
+    "url": "/swarup_website/static/js/main.b0f7a17d.chunk.js"
+  },
+  {
+    "revision": "34bf9e473db27d30eff1",
+    "url": "/swarup_website/static/js/1.34bf9e47.chunk.js"
+  },
+  {
+    "revision": "be3e530747f371919726",
+    "url": "/swarup_website/static/js/runtime~main.be3e5307.js"
+  },
+  {
+    "revision": "437423e0b45baa629e6f09e618036f4f",
+    "url": "/swarup_website/static/media/CV.437423e0.pdf"
+  },
+  {
+    "revision": "9bbff334bee2d53b736132e32b88c2e0",
+    "url": "/swarup_website/static/media/MyPic.9bbff334.jpg"
+  },
+  {
+    "revision": "181d94865dcc0c68795cc03dc63d7f62",
+    "url": "/swarup_website/static/media/portfolio1.181d9486.jpg"
+  },
+  {
+    "revision": "1164e9f9f8d53fb50d11379f6723fc7a",
+    "url": "/swarup_website/static/media/portfolio2.1164e9f9.jpg"
+  },
+  {
+    "revision": "58a798691b01c08078753c62a166c18d",
+    "url": "/swarup_website/static/media/portfolio3.58a79869.jpg"
+  },
+  {
+    "revision": "1b18d4b4a632cdd4ff29eb805d581269",
+    "url": "/swarup_website/index.html"
+  }
+];
