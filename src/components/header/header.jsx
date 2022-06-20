19a84edfcc8133bc168387dfb6122c1553e6968a
Swarup Zachariah <Swarup.zac@gmail.com>
2022-06-20 10:50:39 -0400

    Fixed css for mobile, tablet, and websites

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -10,15 +10,17 @@ const Header = () => {
         <div className="container header__container">
           <h3> Hello I am</h3>
           <h2>Swarup Alex Zachariah</h2>
-          <p className='about_me'>
-            A Fullstack Developer and hiking enthusiast. After 8 years in the
-            software engineering industry with experiences in design and
-            testing, I am transitioning careers to move into software
-            development. Scroll below to learn more about my skills and
-            endeavors from the recent past.
-          </p>
+          <div className="about_me">
+            <h3>
+              A Fullstack Developer and hiking enthusiast. After 8 years in the
+              software engineering industry with experiences in design and
+              testing, I am transitioning careers to move into software
+              development. To learn more about my skills and
+              endeavors from the recent past, scroll below.
+            </h3>
+          </div>
         </div>
-        <div className='CTA'>
+        <div className="CTA">
           <CTA />
         </div>
         <HeaderSocials />
